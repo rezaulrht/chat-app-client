de14d7829524490eaa6a34619a937ed1064dc047
rezaulkarim <rezaulrahaat@gmail.com>
2026-02-17 00:14:26 +0600

    feat: add new components to HomePage and update dependencies

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
 import React from "react";
 import CustomerReview from "./about/review/page";
+import HeroSection from "@/components/Landing/HeroSection";
+import Features from "@/components/Landing/Features";
+import UseCases from "@/components/Landing/UseCases";
+import Integrations from "@/components/Landing/Integrations";
+import CompetitiveEdge from "@/components/Landing/CompetitiveEdge";
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <HeroSection />
+      <Features />
+      <UseCases />
+      <Integrations />
+      <CompetitiveEdge />
       <CustomerReview />
     </div>
   );

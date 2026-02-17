3ba290188c62ece16cddd45854d10eca1460eccc
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2026-02-17 22:21:20 +0600

    fix: CreateGroupModal.jsx implement and next/image error — allowed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+        port: "", 
+        pathname: "/**", 
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import CustomerReview from "./about/review/page";
 import Banner from "./about/banner/page";
+import CreateGroupModal from "@/components/CreateGroupModal";
 
 export default function HomePage() {
   return (
     <div>
       <Banner/>
       <CustomerReview />
+      <CreateGroupModal/>
       <h1>Home Page</h1>
     </div>
   );

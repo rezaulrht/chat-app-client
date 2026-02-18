047b4b3f2ef7fd3c5e9b56f6b9a40215a62379c0
Toufiqul2022 <toufiqul8865@gmail.com>
2026-02-19 00:23:47 +0600

    feat: add our story and CTA Section

--- src/app/AboutUs/page.js
+++ src/app/AboutUs/page.js
@@ -1,10 +1,12 @@
 // src/app/about/page.jsx
-import AboutSection from "@/components/AboutUs/AboutSection";
+import AboutSection from "@/components/AboutUs/Hero&Features";
+import StoryAndCTA from "@/components/AboutUs/Story&CTA";
 
 export default function AboutPage() {
   return (
     <main>
       <AboutSection />
+      <StoryAndCTA />
     </main>
   );
 }

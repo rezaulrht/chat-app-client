48bc8fafd5bd6f25882f564f588eb31062ba4c97
Toufiqul2022 <toufiqul8865@gmail.com>
2026-02-19 00:07:29 +0600

    feat: add AboutUs route and about Hero and features section design

---
+++ src/app/AboutUs/page.js
@@ -0,0 +1,10 @@
+// src/app/about/page.jsx
+import AboutSection from "@/components/AboutUs/AboutSection";
+
+export default function AboutPage() {
+  return (
+    <main>
+      <AboutSection />
+    </main>
+  );
+}

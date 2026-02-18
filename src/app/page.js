261d1311c3726bf7c0bd02257823d1272cad1f89
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2026-02-18 14:26:39 +0600

    some errer fixes

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,11 @@ import React from "react";
 import CustomerReview from "./about/review/page";
 import Banner from "./about/banner/page";
 import CreateGroupModal from "@/components/CreateGroupModal";
+import Sidebar from "@/components/Sidebar";
 
 export default function HomePage() {
   return (
     <div>
-      <Banner/>
       <CustomerReview />
       <CreateGroupModal/>
       <h1>Home Page</h1>

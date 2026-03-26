358bd2c0dc7b94f0d8e63003287265c69985ed09
Md. Rezaul Karim <rezaulrahaat@gmail.com>
2026-03-26 11:04:18 +0600

    feat: add AppTopBar and AppShellProvider to app layout

--- src/app/app/layout.jsx
+++ src/app/app/layout.jsx
@@ -1,6 +1,19 @@
 "use client";
 import { SocketProvider } from "@/context/SocketProvider";
+import { AppShellProvider } from "@/components/app-shell/AppShellContext";
+import AppTopBar from "@/components/app-shell/AppTopBar";
 
 export default function AppLayout({ children }) {
-  return <SocketProvider>{children}</SocketProvider>;
+  return (
+    <AppShellProvider>
+      <SocketProvider>
+        <div className="flex flex-col h-screen overflow-hidden bg-obsidian">
+          <AppTopBar />
+          <div className="flex-1 min-h-0 overflow-hidden">
+            {children}
+          </div>
+        </div>
+      </SocketProvider>
+    </AppShellProvider>
+  );
 }
